32e07a2f3a91880169d088d4dec1437bd7829309
WZS <welwel2@yahoo.com>
2017-06-16 16:58:54 -0600

    commit changes

--- push.py
+++ push.py
@@ -10,8 +10,15 @@ sys.path.append(os.path.split(crn)[0])
 #print sys.path
 from cr import update_remote
 
-#os.system(r'python clean.py')
+print "call script to clean directory"
+os.system(r'python clean.py')
+
+print "issue git command to remove unwated files"
+os.system(r'git rm *.py[oc]')
+
+print "call update_remote to commit changes, and push to server"
 update_remote(os.getcwd(), 'ltv')
 
+print "call script to update wsrepo repository on github server"
 #os.system(r'python %s'%crn)
 
