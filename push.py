fbde9af50a55df6805d1d273153740d3cac00ee5
WZS <welwel2@yahoo.com>
2017-06-16 17:28:19 -0600

    another minor change to push.py

--- push.py
+++ push.py
@@ -19,10 +19,13 @@ os.system(r'git rm *.py[oc]')
 print "issue git command to add modified files"
 os.system(r'git add *')
 
-print "issue git command to add modified files"
+print "issue git command to commit changes"
 comment = raw_input('enter a good description for your changes: ')
 os.system(r'git commit -m "%s"'%comment)
 #update_remote(os.getcwd(), 'ltv')
+
+print "issue git command to push changes to server"
+os.system(r'git push')
 
 print "call script to update wsrepo repository on github server"
 #os.system(r'python %s'%crn)
