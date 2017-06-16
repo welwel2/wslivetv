c4e6bb2ff26f4744c435ca0e25e3d8fbd5a74890
WZS <welwel2@yahoo.com>
2017-06-16 15:44:02 -0600

    commit changes

---
+++ push.py
@@ -0,0 +1,16 @@
+# This scrip will commit the latest changes, and then, push the changes to 
+# remote server.
+# Call the cr script to update the wsrepo on remote server.
+import sys
+import os
+
+crn = r'C:\Users\220554\Documents\GitHub\wsrepo\_tools\cr.py'
+
+sys.path.append(os.path.split(crn)[0])
+print sys.path
+from cr import update_remote
+
+update_remote(os.getcwd())
+
+os.system(r'python %s'%crn)
+
